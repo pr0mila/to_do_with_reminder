e205ac5a17807bd2a7295bdbb5fa882a824d8531
monty <me@promila.info>
2021-08-07 17:44:18 +0600

    Checklist

--- lib/main.dart
+++ lib/main.dart
@@ -11,62 +11,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.brown,
       ),
-      home: Home(),
+      home: MyHomePage(),
     );
   }
 }
-class Home extends StatefulWidget {
-  @override
-  HomeState createState() => HomeState();
-}
-class HomeState extends State<Home> {
-  List<Todo>list = <Todo>[];
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Todo>list = <Todo>[];
   @override
   void initState() {
-    // TODO: implement initState
     list.add(Todo(title: 'Item A'));
     list.add(Todo(title: 'Item B'));
     list.add(Todo(title: 'Item C'));
+    // TODO: implement initState
     super.initState();
   }
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tasks in progress'),
-      ),
+     appBar: AppBar(
+       title: Text('My Tasks on progress'),
+       centerTitle: true,
+     ),
       body: ListView.builder(
-       itemCount: list.length,
-        itemBuilder: (context,index){
-         return ListTile(
-           title: Text(list[index].title),
-           trailing: Icon(Icons.check_box),
-           onTap: ()=>print(list[index].title),
-         );
-        }
+          itemCount: list.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              title: Text(list[index].title),
+              trailing: Checkbox(value:list[index].complete, onChanged:null),
+              onTap: ()=>setCompleteness(list[index]),
+            );
+          }
       ),
+
     );
   }
+  void setCompleteness(Todo item){
+    setState(() {
+      item.complete =! item.complete;
+    });
+
+  }
 }
-class Todo{
+
+
+class Todo {
   String title;
   bool complete;
-  Todo ({required this.title,
-    this.complete = false});
+  Todo({required this.title,
+  this.complete =  false});
 }
